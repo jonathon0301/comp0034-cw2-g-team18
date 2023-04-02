637e0b1511726183cb789659b246d147195e7b57
JonathonShi <71091633+JonathonShi@users.noreply.github.com>
2023-04-02 22:52:28 +0100

    Add a login fixture and add more tests

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,7 @@
-import time
 import pytest
 from basepage import BasePage
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
